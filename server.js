e097cc2aa1dfee3447443a8050d9809a10d2c3a8
Dev@DESKTOP-U16C9Q3 <kamothowaweru5@gmail.com>
2022-10-23 21:27:53 +0300

    updates

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const Axios = require("axios"); 
-const express = require('express');
-const mongoose = require('mongoose')
-const router = express.Router()
-const db = mongoose.connection
-const Portfolio = require('./apiModel')
+const Axios = require("axios");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const db = mongoose.connection;
+const Portfolio = require("./apiModel");
 
 const options = {
   method: "GET",
@@ -14,25 +14,28 @@ const options = {
     "X-RapidAPI-Host": "latest-stock-price.p.rapidapi.com",
   },
 };
-mongoose.connect('mongodb://localhost:27017/apidata', {useNewUrlParser: true}, function(err) {
-  if (err) throw err;
-  console.log('success')
-});
-Axios.request(options).then(function(response) {
-  onSuccess(response)
-}).catch(function(error) {
-  console.log(error);
-});
-
+mongoose.connect(
+  "mongodb://localhost:27017/apidata",
+  { useNewUrlParser: true },
+  function (err) {
+    if (err) throw err;
+    console.log(" Connection successful");
+  }
+);
+Axios.request(options)
+  .then(function (response) {
+    onSuccess(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 const apiSchema = mongoose.Schema({
   symbol: {
-    type: String
-    
+    type: String,
   },
   identifier: {
-    type: String
-    
+    type: String,
   },
   open: {
     type: String,
@@ -68,19 +71,19 @@ const apiSchema = mongoose.Schema({
     type: String,
   },
   perecentagechange365d: {
-    type: String
+    type: String,
   },
   perecentagechange30d: {
-    type: String
-  }
+    type: String,
+  },
 });
-const Data = mongoose.model('Data', apiSchema);
+const Data = mongoose.model("Data", apiSchema);
 
 function onSuccess(response) {
   const array = response;
-  const arrayLength = Object.keys(array).length
-  console.log(arrayLength)
-  for( i=0; i<=arrayLength; i++) {
+  const arrayLength = Object.keys(array).length;
+  console.log(arrayLength);
+  for (i = 0; i <= arrayLength; i++) {
     const symbol = array.data[i].symbol;
     const identifier = array.data[i].identifier;
     const open = array.data[i].open;
@@ -96,14 +99,44 @@ function onSuccess(response) {
     const yearlow = array.data[i].yearlow;
     const percentagechange365d = array.data[i].percentagechange365d;
     const percentagechange30d = array.data[i].perecentagechange30d;
-    assignDataValue(symbol, identifier, open, dayhigh, daylow, lastprice, previousclose, pchange, totaltradedvolume, totaltradedvalue, lastupdatetime, yearhigh, yearlow, percentagechange365d, percentagechange30d )
-
+    assignDataValue(
+      symbol,
+      identifier,
+      open,
+      dayhigh,
+      daylow,
+      lastprice,
+      previousclose,
+      pchange,
+      totaltradedvolume,
+      totaltradedvalue,
+      lastupdatetime,
+      yearhigh,
+      yearlow,
+      percentagechange365d,
+      percentagechange30d
+    );
   }
-
 }
 
-function assignDataValue(symbol, identifier, open, dayhigh, daylow, lastprice, previousclose, pchange, totaltradedvolume, totaltradedvalue, lastupdatetime, yearhigh, yearlow, percentagechange365d, percentagechange30d) {
-  const uploadData = new Data()
+function assignDataValue(
+  symbol,
+  identifier,
+  open,
+  dayhigh,
+  daylow,
+  lastprice,
+  previousclose,
+  pchange,
+  totaltradedvolume,
+  totaltradedvalue,
+  lastupdatetime,
+  yearhigh,
+  yearlow,
+  percentagechange365d,
+  percentagechange30d
+) {
+  const uploadData = new Data();
   uploadData.symbol = symbol;
   uploadData.identifier = identifier;
   uploadData.open = open;
@@ -119,20 +152,9 @@ function assignDataValue(symbol, identifier, open, dayhigh, daylow, lastprice, p
   uploadData.yearlow = yearlow;
   uploadData.percentagechange365d = percentagechange365d;
   uploadData.percentagechange30d = percentagechange30d;
-  
+
   uploadData.save();
-  
-
-
-
 }
-
-
-
-
-
-
-
 
 {
   /*
@@ -150,8 +172,8 @@ const print = async () => {
   console.log(resp);
 };    
 print();
-*/}
-
+*/
+}
 
 //app.use(cors());
 
@@ -161,7 +183,3 @@ print();
 }).catch(function(error) {
     console.error(error)
 })*/
-
-
-
-  
