fad1c99aa6fe084d071075c3ea57027bc8892c3b
Waweru Kamotho <87232967+wawerutimothy@users.noreply.github.com>
2022-11-01 17:45:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const options = {
     method: "GET",
     url: "https://api.simpleswap.io/get_market_info",
     headers: {
-        "api-Key": "634fa29a31493230724c11cf",
+        
         "accept": "application/json",
     },
 };
